b854edc163ea0625ba47f630ccea693a55df7c28
Flynn <wphestiraid@gmail.com>
2018-11-18 20:34:15 +0900

    Styles in NextJS

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
 import Link from 'next/link';
+import { red } from 'ansi-colors';
+
+const headerStyles = {
+  background: 'red'
+};
 
 export default () => (
-  <header>
+  <header style={headerStyles}>
     <nav>
       <ul>
         <li>
@@ -16,5 +21,17 @@ export default () => (
         </li>
       </ul>
     </nav>
+    <style jsx>{`
+      ul {
+        display: flex;
+      }
+      ul li {
+        margin-right: 20px;
+      }
+      header {
+        background-color: peru;
+        color: white;
+      }
+    `}</style>
   </header>
 );
--- components/PostLink.js
+++ components/PostLink.js
@@ -2,6 +2,15 @@ import Link from 'next/link';
 
 export default props => (
   <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
-    <a>{props.title}</a>
+    <a>
+      {props.title}
+      <style jsx>
+        {`
+          a {
+            background-color: red;
+          }
+        `}
+      </style>
+    </a>
   </Link>
 );
