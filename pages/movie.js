55c7a513f2558b68ed44bf2f7967b46e07e2402a
Flynn <wphestiraid@gmail.com>
2018-11-22 21:18:05 +0900

    Fetching the Movie Details

--- pages/movie.js
+++ pages/movie.js
@@ -1,19 +1,29 @@
 import Head from 'next/head';
 import { withRouter } from 'next/router';
+import axios from 'axios';
 
 const Movie = props => (
   <div>
     <Head>
-      <title>{props.router.query.title} | Nomad Store</title>
+      <title>{props.movie.title} | Nomad Store</title>
     </Head>
-    {JSON.stringify(props)}
-    <h1>{props.title}</h1>
-    <p>lorem ipsum</p>
+    <h1>{props.movie.title}</h1>
+    <p>{props.movie.description_intro}</p>
   </div>
 );
 
-Movie.getInitialProps = async () => {
-  return { title: 'Hey!' };
+Movie.getInitialProps = async ctx => {
+  const {
+    query: { id }
+  } = ctx;
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios.get(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  );
+  return { movie };
 };
 
 export default withRouter(Movie);
