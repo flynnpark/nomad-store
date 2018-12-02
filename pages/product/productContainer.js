dbdb913a91a60bf57d5f636017b2b6d467765db3
Flynn <wphestiraid@gmail.com>
2018-12-02 15:49:08 +0900

    Product Page part Two

--- pages/product/productContainer.js
+++ pages/product/productContainer.js
@@ -1,5 +1,7 @@
 import { withRouter } from 'next/router';
+import { Query } from 'react-apollo';
 import ProductPresenter from './productPresenter';
+import { PRODUCT_QUERY } from './productQueries';
 
 class ProductContainer extends React.Component {
   static async getInitialProps(props) {
@@ -11,7 +13,12 @@ class ProductContainer extends React.Component {
     };
   }
   render() {
-    return <ProductPresenter />;
+    const { id } = this.props;
+    return (
+      <Query query={PRODUCT_QUERY} variables={{ id }}>
+        {({ data }) => <ProductPresenter data={data} />}
+      </Query>
+    );
   }
 }
 
